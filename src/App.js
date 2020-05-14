1c1f08877708331c9a6d41676d327df6c97b3820
Aaron Miller <aaronmiller822028@gmail.com>
2020-05-14 04:04:26 -0700

    in the future I want to aim to always use docker in my future projects. even in frontend react projects. It should save me from have to spend a long time to spin up a duplicate instances or recycle the instance incase something goes wrong with it. cough cough that time when I typed "npm start" and brought down my prod server... LOL. I think docker images will have more protection from something like this instead of just bare bones pm2 and nginx

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello. My name is <code>Aaron</code>.
+          I need to put this on git ASAP
         </p>
         <a
           className="App-link"
