6ff8067c6a239d4800e1a9f430aed9a561ffc84a
Aaron Miller <aaronmiller822028@gmail.com>
2020-05-15 05:46:23 -0700

    after removing orphans from the docker image I now have all the npm libs working in the dev container

--- src/App.tsx
+++ src/App.tsx
@@ -6,23 +6,10 @@ import ResponsiveDrawer from './components/ResponsiveDrawer';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+      
           <ResponsiveDrawer/>
           Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-
-      </header>
-
+      
       
     </div>
   );
