157d9f9dce80ac6e859ee578a8b0322ede156cb7
Aaron Miller <aaronmiller822028@gmail.com>
2020-05-18 04:01:31 -0700

    this thing were is it not detecting react-router-dom is a annoying waste of time

--- src/components/DrawerLink.ts
+++ src/components/DrawerLink.ts
@@ -2,9 +2,10 @@ import React from 'react'
 
 type DrawerLinkProps = {
     name: string,
-    active: boolean
+    active: boolean,
+    internal: boolean
 }
 
-export const DrawerLink = ({name, active}: DrawerLinkProps) => {
-    return
+export const DrawerLink = ({name, active, internal}: DrawerLinkProps) => {
+    return ;
 }
