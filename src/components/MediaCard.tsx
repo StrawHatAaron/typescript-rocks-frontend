98192daee7d49cd8faa3fb4bdfe4fb055e7a1af4
Aaron Miller <aaronmiller822028@gmail.com>
2020-05-19 03:59:48 -0700

    gonna take a break. I plan to try MediaCard as JSX so it will be less type script to display something relativly simple

--- src/components/MediaCard.tsx
+++ src/components/MediaCard.tsx
@@ -6,6 +6,7 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Chip from '@material-ui/core/Chip';
+
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
@@ -13,7 +14,6 @@ import JavascriptIcon from '../assets/mui/JavascriptIcon';
 import ReactIcon from '../assets/mui/ReactIcon';
 import SwiftIcon from '../assets/mui/SwiftIcon';
 
-import {SvgConverter} from '../utils/SvgConverter';
 
 
 
@@ -23,57 +23,59 @@ const useStyles = makeStyles({
   },
   media: {
     height: 140,
+    paddingTop: '56%'
   },
 });
 
+type colorTypes = 
+  'primary' |
+  'secondary';
 
 
-export default function MediaCard() {
+
+interface ChipProps{
+  label:string,
+  color:colorTypes,
+  avatar:any
+};
+
+interface MediaCardProps{
+  title:string,
+  img:string,
+  explain:string,
+  chip?:[{ 
+    label: string; 
+    color: string; 
+    avatar: (props: any) => Element; 
+  }];
+};
+
+export const MediaCard: React.FC<MediaCardProps> = ({
+title, img, explain, chip}) => {
   const classes = useStyles();
+
+  console.log(img);
 
   return (
     <Card className={classes.root}>
       <CardActionArea>
         <CardMedia
+
           className={classes.media}
-          image="../assets/tCheck.png"
+          image={img}
           title="Contemplative Reptile"
         />
         <CardContent>
           <Typography gutterBottom variant="h5" component="h2">
-            tCheck
+          {title}
           </Typography>
           <Typography variant="body2" color="textSecondary" component="p">
-            Built and launched in both Swift and React for iOS and Android. 
-            These projects where my first industry projects and are still managed to
-            this day. What I did as a software developer for this marijuana petroleum
-            spectroscopy mold firm was quite unique.
+          {explain}
           </Typography>
         </CardContent>
       </CardActionArea>
       <CardContent>
-          <Chip
-              label="Swift"
-              size="small"
-              clickable
-              color="primary"
-              avatar={<SwiftIcon/>}
-            />
-            <Chip
-              label="React"
-              size="small"
-              clickable
-              color="primary"
-              avatar={<ReactIcon/>}
-            />
-            <Chip
-              label="Javascript"
-              size="small"
-              clickable
-              color="primary"
-              avatar={<JavascriptIcon/>}
-            />
-          
+        
       </CardContent>
       <CardActions>
         <Button size="small" color="primary">
--- src/views/Portfolio.tsx
+++ src/views/Portfolio.tsx
@@ -1,10 +1,44 @@
 import React from 'react';
-import MediaCard from '../components/MediaCard'
+import Grid from '@material-ui/core/Grid'
+import {MediaCard} from '../components/MediaCard'
+import JavascriptIcon from '../assets/mui/JavascriptIcon';
+import ReactIcon from '../assets/mui/ReactIcon';
+import SwiftIcon from '../assets/mui/SwiftIcon';
+import tCheckImage from '../assets/img/tCheck.png'
+
+const portfolioRoutes = [{
+  title:"tCheck",
+  img:tCheckImage,
+  explain:"Built and launched in both Swift and React for iOS and Android. These projects where my first industry projects and are still managed to this day. What I did as a software developer for this marijuana petroleum spectroscopy mold firm was quite unique.",
+  chip:[{
+    label:"Swift",
+    color:"primary",
+    avatar:() => JavascriptIcon
+  },{
+    label:"React",
+    color:"primary",
+    avatar:() => ReactIcon
+  },{
+    label:"Javascript",
+    color:"primary",
+    avatar:() => SwiftIcon
+  }],
+}]
 
 export default function Portfolio() {
   return (
-    <>
-      <MediaCard/>
-    </>
+    <Grid container spacing={3}>
+      {portfolioRoutes.map((c, i) =>(
+        <Grid item xs={12} sm={6}>
+          <MediaCard 
+            key={"media-resume-card"+i}
+            title={c.title}
+            img={c.img}
+            explain={c.explain}
+
+            />
+        </Grid>
+      ))}
+    </Grid>
   );
 }
