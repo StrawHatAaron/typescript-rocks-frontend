71c6c91a53ef39ddd91dfb7d38a7b71a3d0a4d40
Aaron Miller <aaronmiller822028@gmail.com>
2020-12-22 03:38:46 -0800

    Updates

---
+++ precache-manifest.4f7f686da8aad46e7359fec613f12944.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1aaf48ddad11313c5adfbbbb47d99ad0",
+    "url": "/typescript-rocks-frontend/index.html"
+  },
+  {
+    "revision": "6484b6c36f470a2c1b27",
+    "url": "/typescript-rocks-frontend/static/css/main.abf36e19.chunk.css"
+  },
+  {
+    "revision": "0d40f8f9c3b7797b0e24",
+    "url": "/typescript-rocks-frontend/static/js/2.81909354.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/typescript-rocks-frontend/static/js/2.81909354.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6484b6c36f470a2c1b27",
+    "url": "/typescript-rocks-frontend/static/js/main.5eb61bb4.chunk.js"
+  },
+  {
+    "revision": "4642bac29c7693b2ea7e",
+    "url": "/typescript-rocks-frontend/static/js/runtime-main.e41478c4.js"
+  },
+  {
+    "revision": "4910d33c8e0c653228c2292f74528909",
+    "url": "/typescript-rocks-frontend/static/media/BuildTheLanes.4910d33c.png"
+  },
+  {
+    "revision": "a4d612d0557ca27c38e60e662bfaf7dd",
+    "url": "/typescript-rocks-frontend/static/media/Duplicagent.a4d612d0.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/typescript-rocks-frontend/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- src/react-app-env.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference types="react-scripts" />
-
-declare module '@types/react-router-dom';
