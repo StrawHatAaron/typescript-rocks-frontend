1fb7abcbce7dc382f52d7245a1b9d80c396c7b7f
StrawHatAaron <aaronmiller822028@gmail.com>
2021-11-07 01:09:50 -0800

    Updates

---
+++ precache-manifest.7f0d44d162df56ac1c663aebb874bd8c.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa95f319ac943ca8bd34aa604f0cab0b",
+    "url": "/typescript-rocks-frontend/index.html"
+  },
+  {
+    "revision": "8d36523ef4dbe750867a",
+    "url": "/typescript-rocks-frontend/static/css/main.abf36e19.chunk.css"
+  },
+  {
+    "revision": "595a5f8e16c48e06606b",
+    "url": "/typescript-rocks-frontend/static/js/2.4bb2dc6a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/typescript-rocks-frontend/static/js/2.4bb2dc6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d36523ef4dbe750867a",
+    "url": "/typescript-rocks-frontend/static/js/main.c3b61226.chunk.js"
+  },
+  {
+    "revision": "4642bac29c7693b2ea7e",
+    "url": "/typescript-rocks-frontend/static/js/runtime-main.e41478c4.js"
+  },
+  {
+    "revision": "4910d33c8e0c653228c2292f74528909",
+    "url": "/typescript-rocks-frontend/static/media/BuildTheLanes.4910d33c.png"
+  },
+  {
+    "revision": "851753695d48d646960ae04fb18ddd7b",
+    "url": "/typescript-rocks-frontend/static/media/DesignerWebs.85175369.png"
+  },
+  {
+    "revision": "a4d612d0557ca27c38e60e662bfaf7dd",
+    "url": "/typescript-rocks-frontend/static/media/Duplicagent.a4d612d0.png"
+  },
+  {
+    "revision": "b23c35cc2341b2289fea00d9b22b50d8",
+    "url": "/typescript-rocks-frontend/static/media/LetItGo.b23c35cc.png"
+  },
+  {
+    "revision": "bc3dcf00d0fe22f909716354ec0f2e38",
+    "url": "/typescript-rocks-frontend/static/media/foundation.bc3dcf00.png"
+  },
+  {
+    "revision": "bb106cfd70bd45e4c69c3a38258a394a",
+    "url": "/typescript-rocks-frontend/static/media/javascript-academy.bb106cfd.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/typescript-rocks-frontend/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1c609496609a20b60438084e719ec27d",
+    "url": "/typescript-rocks-frontend/static/media/reactive-nerd.1c609496.png"
+  },
+  {
+    "revision": "5d34e460a43464cc193f99159398f69e",
+    "url": "/typescript-rocks-frontend/static/media/solar-leads.5d34e460.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.64b9818e8a63b80ab1d89ff32127621f.js"
+  "/typescript-rocks-frontend/precache-manifest.7f0d44d162df56ac1c663aebb874bd8c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/typescript-rocks-frontend/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
