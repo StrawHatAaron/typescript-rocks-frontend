cdcff9dcf8ed207f29cb499b9fe6fa4a363ef40c
StrawHatAaron <aaronmiller822028@gmail.com>
2021-11-08 01:49:01 -0800

    fixed the DNS issue

--- src/utils/Routes.ts
+++ src/utils/Routes.ts
@@ -1,51 +1,60 @@
-import Home from '../views/Home';
-import Portfolio from '../views/Portfolio';
-import {Contact} from '../views/Contact';
-import Blog from '../views/Blog';
-import Settings from '../views/Settings';
-import HomeIcon from '@material-ui/icons/Home';
-import AssignmentIcon from '@material-ui/icons/Assignment';
-import MailIcon from '@material-ui/icons/Mail';
-import LinkedInIcon from '@material-ui/icons/LinkedIn';
-import GitHubIcon from '@material-ui/icons/GitHub';
-import BookIcon from '@material-ui/icons/Book';
-import SettingsIcon from '@material-ui/icons/Settings';
+import Home from "../views/Home";
+import Portfolio from "../views/Portfolio";
+import { Contact } from "../views/Contact";
+import Blog from "../views/Blog";
+import Settings from "../views/Settings";
+import HomeIcon from "@material-ui/icons/Home";
+import AssignmentIcon from "@material-ui/icons/Assignment";
+import MailIcon from "@material-ui/icons/Mail";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import BookIcon from "@material-ui/icons/Book";
+import SettingsIcon from "@material-ui/icons/Settings";
 
-const baseRoute = "/";
+const baseRoute = "/typescript-rocks-frontend/";
 
-export const internalLandingRoutes = [{
-    name: 'Home',
-    route:baseRoute+'',
+export const internalLandingRoutes = [
+  {
+    name: "Home",
+    route: baseRoute + "",
     view: Home,
     icon: HomeIcon,
-},{
-    name: 'Portfolio',
-    route:baseRoute+'Portfolio',
+  },
+  {
+    name: "Portfolio",
+    route: baseRoute + "Portfolio",
     view: Portfolio,
     icon: AssignmentIcon,
-},{
-    name: 'Contact',
-    route:baseRoute+'Contact',
+  },
+  {
+    name: "Contact",
+    route: baseRoute + "Contact",
     view: Contact,
     icon: MailIcon,
-},{
-    name: 'Blog',
-    route:baseRoute+'Blog',
+  },
+  {
+    name: "Blog",
+    route: baseRoute + "Blog",
     view: Blog,
     icon: BookIcon,
-},{
-    name:'Settings',
-    route:baseRoute+'Settings',
-    view:Settings,
-    icon:SettingsIcon
-}];
+  },
+  {
+    name: "Settings",
+    route: baseRoute + "Settings",
+    view: Settings,
+    icon: SettingsIcon,
+  },
+];
 
-export const externalLandingRoutes = [{
-    name:"LinkedIn",
+export const externalLandingRoutes = [
+  {
+    name: "LinkedIn",
     href: "https://www.linkedin.com/in/strawhataaron",
-    icon: LinkedInIcon
-},{
-    name:"GitHub",
-    href:"https://github.com/strawhataaron",
-    icon:GitHubIcon
-}]
+    icon: LinkedInIcon,
+  },
+  {
+    name: "GitHub",
+    href: "https://github.com/strawhataaron",
+    icon: GitHubIcon,
+  },
+];
